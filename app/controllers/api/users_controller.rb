2e558a56046c33493674c41b57e60feed3d05801
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-05 19:14:09 -0500

    user controller complete and tested

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,35 +2,30 @@ class Api::UsersController < ApplicationController
 
   wrap_parameters include: User.attribute_names + ["password"]
 
-  # before_action :require_logged_in, only: [:update, :show]
-  # before_action :require_logged_out, only: [:create]
+  before_action :require_logged_in, only: [:update, :show]
+  before_action :require_logged_out, only: [:create]
 
   def create
     @user = User.new(user_params)
 
     if @user.save
       login!(@user)
-      render json: @user
+      render :show
     else 
       render json: { errors: @user.errors.full_messages }, status: 422
     end
   end
   
   def show
-    @user = User.find_by(id: params[:id])
-
-    if @user 
-      render json: @user
-    else 
-      render json: { errors: @user.full_messages }, status: 422
-    end 
+    @user = User.find(params[:id])
+    render json: @user 
   end 
 
   def update
     @user = User.find_by(email: :email)
 
     if @user.save!
-      render @user
+      render :show
     else 
       render json: { errors: @user.errors.full_messages }, status: 422
     end 
@@ -51,15 +46,3 @@ class Api::UsersController < ApplicationController
   end
 
 end
-
-# signupRequestOptions = {
-#   method: 'POST',
-#   headers: { 'Content-Type': 'application/json' },
-#   body: JSON.stringify({ 
-#     email: 'coolemail@hotmail.net', 
-#     first_name: 'cooluser',
-#     last_name: 'mckool',
-#     password: 'starwars',
-#     zipcode: '78613'
-#   })
-# }
