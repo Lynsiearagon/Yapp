a6bd80d393734831f1e06aa7fce658d23d0aa80e
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-07 17:43:21 -0500

    beginning to style login form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import { Redirect } from 'react-router-dom';
+import './LoginForm.css'
 
 
 const LoginFormPage = () => {
@@ -32,30 +33,32 @@ const LoginFormPage = () => {
     }
     
     return (
-        <form onSubmit={handleSubmit}>
-            <ul>
-                {errors.map(error => <li key={error}>{error}</li>)}
-            </ul>
-            <label htmlFor="Email">
-                <input 
-                type="text"
-                placeholder='Email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                />
-            </label>
-            <label htmlFor="Password">
-                <input 
-                type="password"
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                />
-            </label>
-            <button type='submit'>Log In</button>
-        </form>
+        // <div id='form'>
+            <form onSubmit={handleSubmit} id="loginForm">
+                <ul id="errors">
+                    {errors.map(error => <li key={error}>{error}</li>)}
+                </ul>
+                <label htmlFor="Email">
+                    <input 
+                    type="text"
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    />
+                </label>
+                <label htmlFor="Password">
+                    <input 
+                    type="password"
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
+                </label>
+                <button type="submit">Log In</button>
+            </form>
+
     )
 };
 
