254835b19215b2641901f1dcf99df7c7190b1e9c
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-07 15:57:03 -0500

    half way through LoginFormPage

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,15 +1,48 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react';
+import * as sessionActions from '../../store/session';
+import { Redirect } from 'react-router-dom';
+import { getSessionUser } from '../../store/session';
+
+
+
 const LoginFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(getSessionUser());
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return <Redirect to='/' />
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors([]);
+        return dispatch(sessionActions.login({email, password}))
+            .catch(async (res) => {
+                let data; 
+                try {
+                data = await res.clone().json();
+                } catch {
+                    data = await res.text(); 
+                }
+                if (data?.errors) setErrors(data.errors);
+                else if (data) setErrors([data]); 
+                else setErrors([res.statusText]);
+                
+            });
+    }
+    
+
     return (
-        <>
-            <form>
-                <label>Email
-                    <input type="text"/>
-                </label>
-                <label>Password
-                    <input type="password"/>
-                </label>
-            </form>
-        </>
+        <form>
+            <label>Email
+                <input type="text"/>
+            </label>
+            <label>Password
+                <input type="password"/>
+            </label>
+        </form>
     )
 };
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,49 +3,57 @@ import csrfFetch from './csrf';
 const SET_CURRENT_USER = 'session/setCurrentUser';
 const REMOVE_CURRENT_USER = 'session/removeCurrentUser';
 
-const setCurrentUser = (user) => ({
-    type: SET_CURRENT_USER,
-    user
+export const setCurrentUser = (user) => ({
+  type: SET_CURRENT_USER,
+  user
 });
 
-const removeCurrentUser = () => ({
-    type: REMOVE_CURRENT_USER
+export const removeCurrentUser = () => ({
+  type: REMOVE_CURRENT_USER
 });
 
-const storeCSRFToken = (response) => {
-    const csrfToken = response.headers.get("X-CSRF-Token");
-    if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
+export const getSessionUser = (state) => {
+  if (state.session.user) {
+    return state.session.user
+  } else {
+    return null
+  };
+};
+
+export const storeCSRFToken = (response) => {
+  const csrfToken = response.headers.get("X-CSRF-Token");
+  if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
 };
   
-const storeCurrentUser = (user) => {
-    if (user) sessionStorage.setItem("currentUser", JSON.stringify(user));
-    else sessionStorage.removeItem("currentUser");
+export const storeCurrentUser = (user) => {
+  if (user) sessionStorage.setItem("currentUser", JSON.stringify(user));
+  else sessionStorage.removeItem("currentUser");
 };
 
 export const signUpUser = (user) => async (dispatch) => {
-    const { email, first_name, last_name, password, zipcode, birthday} = user;
-    const res = await csrfFetch("/api/users", {
-        method: "POST", 
-        body: JSON.stringify({
-            email,
-            first_name, 
-            last_name,
-            password,
-            zipcode,
-            birthday
-        })
-    });
-    const data = await res.json();
-    sessionStorage.setItem("currentUser", JSON.stringify(user.data))
-    dispatch(setCurrentUser(data.user));
+  const { email, first_name, last_name, password, zipcode, birthday} = user;
+  const res = await csrfFetch("/api/users", {
+    method: "POST", 
+    body: JSON.stringify({
+      email,
+      first_name, 
+      last_name,
+      password,
+      zipcode,
+      birthday
+    })
+  });
+  const data = await res.json();
+  sessionStorage.setItem("currentUser", JSON.stringify(user.data))
+  dispatch(setCurrentUser(data.user));
 };  
 
 export const login = ({ email, password }) => async (dispatch) => {
   const res = await csrfFetch('/api/session', {
     method: 'POST',
     body: JSON.stringify({
-        email,
-        password
+      email,
+      password
     })
   });
   const data = await res.json();
@@ -55,25 +63,25 @@ export const login = ({ email, password }) => async (dispatch) => {
 };
 
 export const restoreSession = () => async dispatch => {
-    const res = await csrfFetch("/api/session");
-    storeCSRFToken(res);
-    const data = await res.json();
-    storeCurrentUser(data.user);
-    dispatch(setCurrentUser(data.user));
-    return res;
+  const res = await csrfFetch("/api/session");
+  storeCSRFToken(res);
+  const data = await res.json();
+  storeCurrentUser(data.user);
+  dispatch(setCurrentUser(data.user));
+  return res;
 };
 
 export const logoutUser = () => async (dispatch) => {
-    const res = await csrfFetch('/api/session', {
-        method: 'DELETE'
-    });
-    storeCurrentUser(null);
-    dispatch(removeCurrentUser());
-    return res
+  const res = await csrfFetch('/api/session', {
+    method: 'DELETE'
+  });
+  storeCurrentUser(null);
+  dispatch(removeCurrentUser());
+  return res
 };
 
 const initialState = { 
-    user: JSON.parse(sessionStorage.getItem("currentUser"))
+  user: JSON.parse(sessionStorage.getItem("currentUser"))
 };
 
 const sessionReducer = (state = initialState, action) => {
