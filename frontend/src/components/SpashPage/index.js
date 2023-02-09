e14aa2d6f6ca4cd3109157e6180329feda8e4c60
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-09 10:04:14 -0500

    Spash page component created

---
+++ frontend/src/components/SpashPage/index.js
@@ -0,0 +1,12 @@
+
+
+const FlashPage = () => {
+
+    return (
+
+        <div>
+            <img src="https://www.smakslykke.no/assets/img/1024/1024/bilder_nettbutikk/87540203c8ae575ec77be9641daf25aa-image.jpeg" alt="spred of tapas" />
+        </div>
+
+    )
+}
