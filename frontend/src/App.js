f91b3a74ea4beba5298609c422997c8b28133b87
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-09 08:57:13 -0500

    Logo and functional buttons in nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import {Route, Switch} from 'react-router-dom';
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import Navigation from "./components/Navigation";
-import SessionNavigation from "./components/Navigation/_navigationUtils";
 
 
 function App() {
@@ -16,9 +15,6 @@ function App() {
           </Route>
           <Route path='/signup'>
             <SignupFormPage />
-          </Route>
-          <Route>
-            <SessionNavigation />
           </Route>
         </Switch>
     </>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,16 +2,20 @@ import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import React from 'react';
 import './Navigation.css';
+import SessionNavigation from './_navigationUtils';
 
 const Navigation = () => {
     const location = useLocation();
 
     return (
-        <Link>
-            {location.pathname === '/' ? 
-            <img src={require("../../images/whiteYappLogo.png")} alt="Logo" className="logo"/> : 
-            <img src={require("../../images/blackRedYappLogo.png")} alt="Logo" className="logo"/> }
-        </Link>
+        <nav id="navBar">
+            <Link to="/">
+                {location.pathname === '/' ? 
+                <img src={require("../../images/whiteYappLogo.png")} alt="Logo" className="logo"/> : 
+                <img src={require("../../images/blackRedYappLogo.png")} alt="Logo" className="logo"/> }
+            </Link>
+            <SessionNavigation />
+        </nav>
     )
 };
 
