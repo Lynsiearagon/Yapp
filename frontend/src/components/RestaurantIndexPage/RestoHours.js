f5ca0f4936fb0b6b8e6dfec91b3e0cdbb0a42d7e
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-03-17 16:47:14 -0400

    Update seed hour format

--- frontend/src/components/RestaurantIndexPage/RestoHours.js
+++ frontend/src/components/RestaurantIndexPage/RestoHours.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
 
 export const RestoHoursSingleLine = ({times}) => {
