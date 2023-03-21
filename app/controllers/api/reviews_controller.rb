04820b269f45a9295f88d50e67d52de566482c0f
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-03-21 13:49:54 -0400

    Review Controller tested and complete, Routes complete

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -9,8 +9,7 @@ class Api::ReviewsController < ApplicationController
         @review.reviewer_id = current_user.id 
 
         if @review.save
-            render '/api/restaurants/show'
-            # redirect_to restaurant_url(@review.restaurant_id)
+            redirect_to api_restaurant_url(@review.restaurant_id)
         else
             render json: { errors: @review.errors.full_messages}, status: 422
         end
@@ -24,8 +23,7 @@ class Api::ReviewsController < ApplicationController
         @review = Review.find_by(id: params[:id]) 
 
         if @review.reviewer_id == current_user.id && review.update
-            render '/api/restaurants/show'
-            # redirect_to restaurant_url(@review.restaurant_id)
+            redirect_to api_restaurant_url(@review.restaurant_id)
         else 
             render json: { errors: @review.errors.full_messages}, status: 422 
         end
@@ -38,14 +36,15 @@ class Api::ReviewsController < ApplicationController
         if @review && @review.reviewer_id == current_user.id
             @review.destroy
         end
-        render '/api/restaurants/show'
+        
+        redirect_to api_restaurant_url(@review.restaurant_id)
     end 
 
 
     private 
 
     def review_params
-        params.require(:review).permit(:body, :star_rating)
+        params.require(:review).permit(:body, :star_rating, :reviewer_id, :restaurant_id)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resource :session, only: [:create, :show, :destroy]
     resources :restaurants, only: [:index, :show]
+    resources :reviews, only: [:create, :index, :show, :update, :destroy]
   end
 
   get '*path', to: "static_pages#frontend_index"
