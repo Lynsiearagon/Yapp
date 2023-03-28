07f79c047dd2ca0b8e7839fccd8e814407db27b3
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-03-28 18:34:02 -0400

    Working on dynamic star calculator/imaging

---
+++ frontend/src/components/Stars/Index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BsStarFill } from 'react-icons/bs';
+
+
+export const VisualAverageStarRating = (restaurant) => {
+
+    
+
+    let average = Math.floor()
+
+    switch(average) {
+        case 0 || null:
+            return  <>
+                    <BsStarFill className="greyStars"/>
+                    <BsStarFill className="greyStars"/>
+                    <BsStarFill className="greyStars"/>
+                    <BsStarFill className="greyStars"/>
+                    <BsStarFill className="greyStars"/>
+                    </>
+    }
+
+};
+
+// export const VisualUserStarRating = () => {
+
+// }
+
+
