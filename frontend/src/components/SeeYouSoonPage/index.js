c2ba5b6c5a7df4afab9bf88361fd5a6e3b40002f
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-04-11 18:41:33 -0400

    Only render seeyousoon page when logged out

--- frontend/src/components/SeeYouSoonPage/index.js
+++ frontend/src/components/SeeYouSoonPage/index.js
@@ -1,37 +1,45 @@
 import React from 'react'
 import './SeeYouSoon.css'
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const SeeYouSoonPage = () => {
+    const sessionUser = useSelector(state => state.session.user);
+    const history = useHistory();
 
-    return (
-        <div id="formAndImageWrapperSYS">
-            <div id="contactMeDiv">
-            <header id="seeYouSoonHeaderDiv">
-                <h2 id="seeYouSoonHeader">
-                    Heading out? <br />
-                    Thanks for visiting.
-                </h2>
-                <p id="availability">
-                    Like what you see? Please feel free to contact me. 
-                    <br /> I'm available Monday - Friday from 9 am to 7 pm.
-                </p>
-            </header>
-                <p id="contactMessage">
-                    Contact information listed below:
-                </p>
-                <input 
-                    type="text" 
-                    id="contactInfo"
-                    placeholder=" +1 (512) 736-8054  or  Lynsie.aragon@gmail.com" />
+    if (sessionUser) {
+        history.push("/")
+    } else {
+        return (
+            <div id="formAndImageWrapperSYS">
+                <div id="contactMeDiv">
+                <header id="seeYouSoonHeaderDiv">
+                    <h2 id="seeYouSoonHeader">
+                        Heading out? <br />
+                        Thanks for visiting.
+                    </h2>
+                    <p id="availability">
+                        Like what you see? Please feel free to contact me. 
+                        <br /> I'm available Monday - Friday from 9 am to 7 pm.
+                    </p>
+                </header>
+                    <p id="contactMessage">
+                        Contact information listed below:
+                    </p>
+                    <input 
+                        type="text" 
+                        id="contactInfo"
+                        placeholder=" +1 (512) 736-8054  or  Lynsie.aragon@gmail.com" />
+                </div>
+    
+                <div id="SYSimgDiv">
+                    <img src="https://s3-media0.fl.yelpcdn.com/assets/2/www/img/4a1b6846ee4a/devices/phones.png" 
+                    alt="Yelp app on cellphone screen"
+                    id="seeYouSoonImg" />
+                </div>
             </div>
+        )
+    }
+};
 
-            <div id="SYSimgDiv">
-                <img src="https://s3-media0.fl.yelpcdn.com/assets/2/www/img/4a1b6846ee4a/devices/phones.png" 
-                alt="Yelp app on cellphone screen"
-                id="seeYouSoonImg" />
-            </div>
-        </div>
-    )
-}
-
-export default SeeYouSoonPage
+export default SeeYouSoonPage;
