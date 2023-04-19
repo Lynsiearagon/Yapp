adc659c1ed1b112743af8db9fad1136a649b885a
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-04-19 11:26:50 -0400

    Update search bar to clear search and search on Enter

--- frontend/src/components/Navigation/SearchBar.js
+++ frontend/src/components/Navigation/SearchBar.js
@@ -1,26 +1,29 @@
-import React, { useState } from "react";
-import { useLocation, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, Link, useHistory } from "react-router-dom";
 import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 import './Navigation.css';
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 const SearchBar = () => {
     const location = useLocation();
     const history = useHistory();
-    const [searchQuery, setSearchQuery] = useState();
+    const [searchQuery, setSearchQuery] = useState('');
+    const clearFilterButton = document.getElementById("clearFiltersButton");
+
+
+    useEffect(() => {
+        if (clearFilterButton) {
+            setSearchQuery('')
+        } 
+    }, [clearFilterButton]);
+
 
     let searchBarShadow; 
 
     if (location.pathname !== '/') {
         searchBarShadow = "0px 0px 20px 0px rgb(195, 193, 193)"
-    }
+    };
 
-    // const handleKeyDown = (event) => {
-    //     if (event.key === 'Enter') {
-    //         setSearchQuery(searchQuery)
-    //     }
-    // }
 
     if ((location.pathname !== '/login') && (location.pathname !== '/signup')) {
 
@@ -35,7 +38,7 @@ const SearchBar = () => {
                     autoComplete="off"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
-                    // onKeyDown={handleKeyDown}
+                    onKeyDown={(e) => e.key === 'Enter' ? history.push(`/restaurants?query=${searchQuery}`) : null}
                     />
                 <Link to={`/restaurants?query=${searchQuery}`} 
                     id="searchButton"
